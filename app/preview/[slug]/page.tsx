10b29e7914351ea134b4f2c230f36ea30ee41a71
Sayan Debnath <sayandebnath@Sayans-MacBook-Air.local>
2026-05-20 20:39:53 +0530

    Added fallback in home page

--- app/preview/[slug]/page.tsx
+++ app/preview/[slug]/page.tsx
@@ -1,5 +1,56 @@
-import StudioPageClient from "@/components/studio/StudioPageClient"
+import { getPageBySlug }
+from "@/lib/contentful/getPage"
 
-export default function StudioPage() {
-  return <StudioPageClient />
+import { sectionRegistry }
+from "@/registry/sectionRegistry"
+
+import UnsupportedSection
+from "@/components/sections/UnsupportedSection"
+
+export default async function PreviewPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+
+  const { slug } = await params
+
+  const page =
+    await getPageBySlug(slug)
+
+  if (!page) {
+    return (
+      <div className="p-10">
+        Page not found
+      </div>
+    )
+  }
+
+  return (
+    <main>
+      {page.sections.map((section) => {
+
+        const Component =
+          sectionRegistry[
+            section.type as keyof typeof sectionRegistry
+          ]
+
+        if (!Component) {
+          return (
+            <UnsupportedSection
+              key={section.id}
+              type={section.type}
+            />
+          )
+        }
+
+        return (
+          <Component
+            key={section.id}
+            {...(section.props as any)}
+          />
+        )
+      })}
+    </main>
+  )
 }
